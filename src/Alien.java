d5c5e3cd0f27f381d634236ceff785b2447506dc
Aaron Toth <aaron@giznaj.com>
2014-03-31 22:22:15 -0400

    25% working.  re-using classes from assignment 1

---
+++ src/Alien.java
@@ -0,0 +1,75 @@
+/**
+ * @author Aaron Toth 300770784
+ * @version 1.0
+ * Final Assignment for Java Intermediate
+ */
+public abstract class Alien
+
+{	
+	/**
+	 * private and protected class members for the application.  only the protected members are directly accessible
+	 * by the derived classes.  the private members need to be accessed by local or overridden public methods.
+	 */
+	protected String name;
+	protected String typeOfAlien;
+	private String homePlanet;
+	private int numberOfEyes;	
+	
+	/**
+	 * overloaded constructor for the class.  any derived class of this class will have to call the super constructor
+	 * and pass it 4 parameters as defined the the constructor of this class
+	 * @param name holds the name String that the alien is given
+	 * @param typeOfAlien represents the type of Alien the object is (Martian or Jupiterian)
+	 * @param homePlanet represents the planet where the alien originates from
+	 * @param numberOfEyes value that holds the number of eyes the alien has
+	 */
+	public Alien(String name, String typeOfAlien, String homePlanet, int numberOfEyes)
+	{
+		this.name = name;
+		this.typeOfAlien = typeOfAlien;
+		this.homePlanet = homePlanet;
+		this.numberOfEyes = numberOfEyes;
+	}
+	
+	/**
+	 * abstract getter method.  To use this method, it has to be implemented from within the derived classes
+	 * @return name which represent the name of the alien
+	 */
+	abstract String getName();
+	
+	/**
+	 * abstract getter method.  To use this method, it has to be implemented from within the derived classes
+	 * @return typeOfAlien which represents the type of alien (Martian or Jupiterian)
+	 */
+	abstract String getTypeOfAlien();
+	
+	/**
+	 * public method that can be used in the super class or overridden in the derived classes.
+	 * @return homePlanet which is used to identify where the alien was born or comes from
+	 */
+	public String getHomePlanet()
+	{
+		return homePlanet;
+	}
+	
+	/**
+	 * public method that can be used in the super class or overridden in the derived classes.
+	 * @return numberOfEyes that represents the number of eyes the alien has
+	 */
+	public int getNumberOfEyes()
+	{
+		return numberOfEyes;
+	}
+	
+	/**
+	 * user defined toString() method that will return more than the default class.toString() method
+	 * @return classDetails.  This is a concatenated string of all the fields of the alien
+	 */
+	public String toString()
+	{
+		String classDetails = getName() + ", a " + getTypeOfAlien() + " is from " + getHomePlanet() + " and has "
+				+ getNumberOfEyes() + " eyes!";
+		
+		return classDetails;
+	}
+}
---
+++ src/Martian.java
@@ -0,0 +1,89 @@
+import java.awt.Graphics;
+
+/**
+ * @author Aaron Toth 300770784
+ * @version 1.0
+ * Final Assignment for Java Intermediate
+ * derived class of the super class (Alien.java) 
+ */
+public class Martian extends Alien
+{
+	final int WIDTH = 50, HEIGHT = 50;
+	final int arcSize = 50;
+	
+	/**
+	 * constructor that calls the super classes constructor.  since the super class constructor has an 
+	 * overloaded constructor that takes parameters (4 of them), then this class when instantiated must call the 
+	 * super class constructor first in it's own constructor.
+	 */
+	public Martian()
+	{
+		super("Mork", "Jupiterian", "Jupiter", 4);
+	}
+	
+	/**
+	 * returns the value stored in the private field 'homePlanet' in the super class.  achieved by calling the 
+	 * super class public getter method.  need to do this for private fields in the super class.
+	 * @return homePlanet a private string in the super class.  Represents the Aliens place of birth
+	 */
+	public String getHomePlanet()
+	{
+		return super.getHomePlanet();
+	}
+	
+	/**
+	 * returns the value stored in the private field 'numberOfEyes' in the super class.  achieved by calling the 
+	 * super class public getter method.  need to do this for private fields in the super class.
+	 * @return numberOfEyes from the super class private field
+	 */
+	public int getNumberOfEyes()
+	{
+		return super.getNumberOfEyes();
+	}
+	
+	/**
+	 * method returns the value stored in the name field.  this method in the derived class has access to the 
+	 * 'name' field in the super class because it has an access modifier of 'protected'
+	 * @param returns the value stored in the 'name' field
+	 */ 
+	public String getName()
+	{
+		return name;
+	}
+	
+	/**
+	 * method returns the value stored in the typeOfAlien field.  this method in the derived class has access to
+	 * the 'typeOfName' field in the super class because it has an access modifier of 'protected'
+	 * @return typeOfAlien String value that is either Martian or Jupiterian
+	 */
+	public String getTypeOfAlien()
+	{
+		return typeOfAlien;
+	}
+	
+	/**
+	 * draws the alien (Martian)
+	 * @param x holds the int value for the starting x coordinate on the grid
+	 * @param y holds the int value for the starting y coordinate on the grid
+	 */
+	public void draw(Graphics pen, int x, int y)
+	{
+		pen.drawRoundRect(x, y, WIDTH, HEIGHT, arcSize, arcSize); //head
+		pen.drawRoundRect((x+10), (y+10), 10, 10, 12, 12); //left eye
+		pen.drawRoundRect((x+30), (y+10), 10, 10, 12, 12); //right eye
+		pen.drawLine((x+10), (y+35), 65, 75); //(start x, start y, finish x, finish y) - mouth
+		pen.drawLine((x+25), y, (x+25), (y-20)); //antenna
+		pen.drawLine((x+15), (y-15), 60, (y-15)); //antenna
+	}
+	
+	/**
+	 * writes the description of the drawings
+	 * @param pen is the graphics object that is used to write the text to the applet
+	 * @param x represents the starting x coordinate for the string
+	 * @param y represents the starting y coordinate for the string
+	 */
+	public void drawString(Graphics pen, int x, int y)
+	{
+		pen.drawString("I'm a Martian", x, y);
+	}
+}
